87cafc1876c3e7fbc983875c87c23e1e536b8030
tsubasa072 <tagwgwat@icloud.com>
2021-04-02 12:06:41 +0900

    DB Connection

---
+++ database/migrations/2021_04_02_120022_create_buy_lists_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBuyListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buy_lists', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->string('name');
+            $table->integer('volume');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buy_lists');
+    }
+}
